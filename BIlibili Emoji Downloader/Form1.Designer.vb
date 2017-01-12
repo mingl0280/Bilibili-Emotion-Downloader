460ab644366cc8572f84192678015e9c78764018
pmx <mingl0280@gmail.com>
2017-01-12 01:57:20 -0700

    Init commit
    Signed-off-by: pmx <mingl0280@gmail.com>

--- BIlibili Emoji Downloader/Form1.Designer.vb
+++ BIlibili Emoji Downloader/Form1.Designer.vb
@@ -22,9 +22,124 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.AllowWebBrowserDrop = False
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(1126, 397)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(877, 408)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Cache"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(1039, 408)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Quit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 413)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "URL"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(41, 410)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(600, 21)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(647, 408)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "GO"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button4.Location = New System.Drawing.Point(958, 408)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Download"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(728, 413)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Download Status"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1126, 443)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "Bilibili Emoji Downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label2 As Label
 End Class
--- BIlibili Emoji Downloader/Form1.vb
+++ BIlibili Emoji Downloader/Form1.vb
@@ -1,3 +1,103 @@
-﻿Public Class Form1
+﻿Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.IO
 
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate("https://www.bilibili.com/video/av7/")
+
+    End Sub
+
+    Private DownList As New List(Of DownloadFileAttr)
+
+    Private Class DownloadFileAttr
+
+        Public Property Uri As Uri
+        Public Property Size As Size
+
+        Public Property Name As String
+        Public Property FileType As String
+
+        Public Property FileName As String
+
+        Public Property Category As String
+
+        Public Property Path As String
+
+        Public Sub New(src As String, title As String)
+            If src.StartsWith("//") Then
+                src = "https:" + src
+            End If
+            Uri = New Uri(src)
+            Size = getSize(src)
+            Name = title.Replace("[", "").Replace("]", "")
+            FileType = getFileType(src)
+            FileName = Name + "." + FileType
+            Category = getCategory(title)
+        End Sub
+
+        Private Function getSize(src As String) As Size
+            Dim R As New Regex("\d\dx\d\d")
+            Dim Rsul = R.Match(src).ToString().Split("x")
+            Return New Size(Val(Rsul(0)), Val(Rsul(1)))
+        End Function
+
+        Private Function getFileType(src As String) As String
+            Return src.Substring(src.LastIndexOf(".") + 1, src.Length - src.LastIndexOf(".") - 1)
+        End Function
+
+        Private Function getCategory(title As String) As String
+            Return title.Substring(1, title.IndexOf("_") - 1)
+        End Function
+
+    End Class
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim CurAddList As New List(Of String)
+        Dim o = WebBrowser1.Document.GetElementsByTagName("img")
+        For Each Img As HtmlElement In o
+            Debug.WriteLine(Img.Parent.GetAttribute("Class"))
+            Debug.WriteLine(Img.Parent.InnerHtml)
+            If Img.Parent.GetAttribute("className") = "emoji-list emoji-icon" Then
+                DownList.Add(New DownloadFileAttr(Img.GetAttribute("src"), Img.GetAttribute("title")))
+                CurAddList.Add(Img.GetAttribute("title"))
+            End If
+        Next
+        Dim str As String = ""
+        For Each item In CurAddList
+            str += item + vbCrLf
+        Next
+        MessageBox.Show("已缓存：" + vbCrLf + str, "提示", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim foldbox As New FolderBrowserDialog()
+        Dim Ret = foldbox.ShowDialog()
+        If Ret = DialogResult.OK Then
+            For Each Item In DownList
+                Item.Path = foldbox.SelectedPath
+                Dim dl As New WebClient
+                AddHandler dl.DownloadDataCompleted, AddressOf onDlEventFinished
+
+                dl.DownloadDataAsync(Item.Uri, Item)
+            Next
+        End If
+    End Sub
+    Private Sub onDlEventFinished(sender As Object, e As DownloadDataCompletedEventArgs)
+        Dim datas() As Byte = e.Result
+        Dim Item As DownloadFileAttr = e.UserState
+        Dim folderpath As String = Item.Path + "\" + Item.Category + "\"
+        If Not Directory.Exists(folderpath) Then
+            Directory.CreateDirectory(folderpath)
+        End If
+        Dim FileFullPath As String = folderpath + Item.FileName
+        Dim fsw As FileStream = File.Create(FileFullPath)
+        fsw.Write(datas, 0, datas.LongLength)
+        fsw.Close()
+        Label2.Text = Item.Name + "Download Complete."
+    End Sub
 End Class
